f3d26e37de214fed8351aed7753fa26aa256289f
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-03-10 14:47:33 +0700

    Database Migration

--- app/src/main/java/com/example/todoapp/util/Util.kt
+++ app/src/main/java/com/example/todoapp/util/Util.kt
@@ -2,11 +2,22 @@ package com.example.todoapp.util
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.todoapp.model.TodoDatabase
 
 val DB_NAME = "newtododb"
 
 fun buildDb(context: Context): TodoDatabase {
-    val db = Room.databaseBuilder(context, TodoDatabase::class.java, DB_NAME).build()
+    val db = Room.databaseBuilder(context, TodoDatabase::class.java, DB_NAME)
+        .addMigrations(MIGRATION_1_2)
+        .build()
     return db
+}
+
+// MIGRATION_1_2 means this migration is used for upgrading the db from version 1 to 2
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("alter table todo add column priority integer default 3 not null")
+    }
 }
