7f8f738a7a2bd8aa24dc14f968586c285cfd2cb6
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-03-10 14:14:56 +0700

    Add Utility

--- app/src/main/java/com/example/todoapp/view/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/view/MainActivity.kt
@@ -2,16 +2,28 @@ package com.example.todoapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.todoapp.R
 import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.fragmentHost) as NavHostFragment).navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
--- app/src/main/java/com/example/todoapp/viewmodel/DetailTodoViewModel.kt
+++ app/src/main/java/com/example/todoapp/viewmodel/DetailTodoViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.room.Room
 import com.example.todoapp.model.Todo
 import com.example.todoapp.model.TodoDatabase
+import com.example.todoapp.util.buildDb
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -17,8 +18,7 @@ class DetailTodoViewModel(application: Application) : AndroidViewModel(applicati
 
     fun addTodo(list: List<Todo>) {
         launch {
-            val db = Room.databaseBuilder(getApplication(), TodoDatabase::class.java, "newtododb")
-                .build()
+            val db = buildDb(getApplication())
 //            *list converts individual element of list into its individual object (Todo object) and set it as separated parameter
             db.todoDao().insertAll(*list.toTypedArray())
         }
